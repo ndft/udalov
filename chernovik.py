4d7a00c1e452641cf3da2dcfff804f3e78153436
Andrey Udalov <39367653+ndft@users.noreply.github.com>
2020-06-01 22:54:41 +0300

    1 project

--- chernovik.py
+++ chernovik.py
@@ -1,1 +1,7 @@
 print('Hello, People! xDD')
+
+# Downloading files from web
+#import requests
+#url = 'https://www.facebook.com/favicon.ico'
+#r = requests.get(url, allow_redirects=True)
+#open('facebook.ico', 'wb').write(r.content)
